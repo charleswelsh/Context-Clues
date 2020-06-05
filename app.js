836e426f5c67b1fa734bce3204eb8680dd4de088
Charles Welsh <charleswelsh@gmail.com>
2020-06-05 10:22:50 -0500

    added functionality

---
+++ app.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var friendsList= [
+        "Dan",
+        "Dash",
+        "Chase",
+        "Ben",
+        "Alex"
+    ];
+
+    var locationList = [
+        "the Bridge",
+        "the Holodeck",
+        "Engineering",
+        "the Captain's Quaters",
+        "the Brig",
+        "the MedBay",
+        "the Transporter Bay",
+        "the Shuttle Bay",
+        "the Mess Hall",
+        "Construction Area Deck 133B",
+    ]
+
+    var weaponList = [
+        "a phaser",
+        "a bat'leth",
+        "Q's stupid logic",
+        "a photon torpedo to the face",
+        "an interstellar gamma ray burst",
+        "some tribbles",
+        "an angry baby Khan",
+        "a time paradox ",
+        "one very lusty Spock",
+        "Borg-induced assimilation",
+        "an 'accidental' fall into the Dilithium core",
+        "an 'unfortunate' transporter miscalculation",
+        "an 'unavoidable' airlock purge",
+        "uncompatible alien babe snoo-snoo",
+        "Romulan Ale poisoning",
+        "a reprogammed synth unit",
+        "the Vulcan Death Pinch",
+        "Whoopi Goldberg",
+        "3D Chess rulebook",
+        "Ferengi Q-tip"
+    ]
+
+    for (var i = 1; i < 100; i++) {
+        var h3 = document.createElement("h3");
+        h3.innerText = `Accusation ${i}`;
+        document.body.appendChild(h3);
+        var details = alertDetails(i)
+        h3.addEventListener("click", details)
+    };
+    
+    function alertDetails(i) {
+        return function() {
+            alert(`Accusation ${i}: I accuse ${friendsList[i % friendsList.length]}, with ${weaponList[i % weaponList.length]} in ${locationList[i % locationList.length]}.`)
+        };
+    };
+});
